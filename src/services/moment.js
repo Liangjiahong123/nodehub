25bb5c73dd1046cf81f7ad02f0360cd0f9ca3607
jiahong <2621762128@qq.com>
2024-04-06 19:06:55 +0800

    feat：查询动态同时返回评论数和详情

--- src/services/moment.js
+++ src/services/moment.js
@@ -1,4 +1,5 @@
-const { Moment, User } = require('../models');
+const { literal } = require('sequelize');
+const { Moment, User, Comment } = require('../models');
 
 class MomentService {
   async create(userId, content) {
@@ -8,28 +9,56 @@ class MomentService {
 
   async findAll(query) {
     const { page, limit } = query;
-    const { count, rows } = await Moment.findAndCountAll({
-      attributes: { exclude: ['UserId', 'userId'] },
-      include: {
-        model: User,
-        attributes: { exclude: ['password'] }
+    let { count, rows } = await Moment.findAndCountAll({
+      attributes: {
+        include: [
+          literal(
+            '(SELECT COUNT(*) FROM comments WHERE comments.momentId = moment.id) AS commentCount'
+          )
+        ]
       },
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ['password'] }
+        }
+      ],
       offset: (page - 1) * limit,
-      limit: +limit
+      limit: +limit,
+      raw: true
     });
-    return { total: count, list: JSON.parse(JSON.stringify(rows)) };
+    rows = rows.map((row) => ({
+      id: row.id,
+      content: row.content,
+      createdAt: row.createdAt,
+      updatedAt: row.updatedAt,
+      deletedAt: row.deletedAt,
+      commentCount: row.commentCount,
+      user: {
+        id: row['user.id'],
+        name: row['user.name']
+      }
+    }));
+    return { momentCount: count, list: JSON.parse(JSON.stringify(rows)) };
   }
 
   async findById(id) {
     const [result] = await Moment.findAll({
       attributes: { exclude: ['userId'] },
       where: { id },
-      include: {
-        model: User,
-        attributes: { exclude: ['password'] }
-      }
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ['password', 'createdAt', 'updatedAt', 'deletedAt'] }
+        },
+        {
+          model: Comment,
+          attributes: { exclude: ['updatedAt', 'deletedAt'] }
+        }
+      ]
     });
-    return result.toJSON();
+    if (result) return result.toJSON();
+    return null;
   }
 
   async update(id, content) {
